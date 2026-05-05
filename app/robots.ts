8c55e3164fa6d1d0b727dd446c4b5383f9e7c8e6
iemtejasvi <stejasvi817@gmail.com>
2026-05-06 01:32:29 +0530

    security: update robots.txt to disallow the new obscure admin route

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/admin', '/api/'],
+        disallow: ['/honey-hq', '/api/'],
       },
     ],
     sitemap: `${SITE_URL}/sitemap.xml`,
