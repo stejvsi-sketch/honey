57fe480c6dfe337c96001045239390a22bae28cd
iemtejasvi <stejasvi817@gmail.com>
2026-05-12 02:03:43 +0530

    Add SEO journal post: The Psychology of Unspoken Words

--- lib/journal-data.ts
+++ lib/journal-data.ts
@@ -11,6 +11,7 @@ import { POSTS_PART2 } from './journal-data-part2';
 import { POSTS_PART3 } from './journal-data-part3';
 import { POSTS_PART4 } from './journal-data-part4';
 import { POSTS_PART5 } from './journal-data-part5';
+import { POSTS_PART6 } from './journal-data-part6';
 
 export const JOURNAL_POSTS: JournalPost[] = [
   ...POSTS_PART1,
@@ -18,4 +19,5 @@ export const JOURNAL_POSTS: JournalPost[] = [
   ...POSTS_PART3,
   ...POSTS_PART4,
   ...POSTS_PART5,
+  ...POSTS_PART6,
 ];
