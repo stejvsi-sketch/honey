fdaf98550315a82e91ca4a45cef0d1a2cf0e643f
iemtejasvi <stejasvi817@gmail.com>
2026-07-12 03:32:01 +0530

    temp: force verification of stubborn zone 11272142 on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -92,6 +92,8 @@ export default async function HomePage() {
       />
       {/* Monetag In-Page Push — Zone for Home */}
       <script dangerouslySetInnerHTML={{ __html: `(function(s){s.dataset.zone='11272143',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))` }} />
+      {/* TEMP: Force verification for stubborn zone */}
+      <script dangerouslySetInnerHTML={{ __html: `(function(s){s.dataset.zone='11272142',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))` }} />
     </>
   );
 }
