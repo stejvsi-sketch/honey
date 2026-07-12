8ec383de387a7d7921eb1f3241e4d3077b58ebdd
iemtejasvi <stejasvi817@gmail.com>
2026-07-12 13:51:19 +0530

    style: make footer copyright text smaller and premium on mobile

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,7 +25,10 @@ export default function Footer() {
           <li><Link href="/moderation" className="footer__link">Moderation Policy</Link></li>
           <li><Link href="/contact" className="footer__link">Contact</Link></li>
         </ul>
-        <p className="footer__copy">&copy; {year} {SITE_NAME}. Founded by <Link href="/author" className="footer__link" style={{textDecoration: 'underline'}}>Tejasvi Sharma</Link>. All letters belong to their authors.</p>
+        <p className="footer__copy">
+          &copy; {year} {SITE_NAME}. Founded by <Link href="/author" className="footer__author">Tejasvi Sharma</Link>.
+          <span className="footer__copy-break">All letters belong to their authors.</span>
+        </p>
       </div>
     </footer>
   );
