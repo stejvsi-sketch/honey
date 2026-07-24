6477767c09a67f1888bb886ff096401606d8e04e
iemtejasvi <stejasvi817@gmail.com>
2026-07-24 12:57:10 +0530

    fix: suppress hydration mismatch warnings on Google AdSense/CMP script tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,9 +62,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* Preconnect to Grow (Mediavine) origin */}
         <link rel="preconnect" href="https://faves.grow.me" crossOrigin="anonymous" />
         {/* Google Funding Choices CMP for GDPR consent (EEA/UK/Switzerland) */}
-        <script async src="https://fundingchoicesmessages.google.com/i/pub-4151123662328725?ers=1"></script>
-        <script dangerouslySetInnerHTML={{ __html: `(function(){function s(){if(!window.frames['googlefcPresent']){if(document.body){var i=document.createElement('iframe');i.style='width:0;height:0;border:none;z-index:-1000;left:-1000px;top:-1000px;';i.style.display='none';i.name='googlefcPresent';document.body.appendChild(i);}else{setTimeout(s,0);}}}s();})();` }} />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4151123662328725" crossOrigin="anonymous"></script>
+        <script async src="https://fundingchoicesmessages.google.com/i/pub-4151123662328725?ers=1" suppressHydrationWarning></script>
+        <script suppressHydrationWarning dangerouslySetInnerHTML={{ __html: `(function(){function s(){if(!window.frames['googlefcPresent']){if(document.body){var i=document.createElement('iframe');i.style='width:0;height:0;border:none;z-index:-1000;left:-1000px;top:-1000px;';i.style.display='none';i.name='googlefcPresent';document.body.appendChild(i);}else{setTimeout(s,0);}}}s();})();` }} />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4151123662328725" crossOrigin="anonymous" suppressHydrationWarning></script>
       </head>
       <body>
         <Navigation />
