2961b8135240c0e712d66aa6fdf807d01b0d94c9
iemtejasvi <stejasvi817@gmail.com>
2026-08-31 21:46:47 +0530

    Fix ad imageWidth to 350 (max) to fill containers properly on all devices

--- components/cards/HomeCardGrid.tsx
+++ components/cards/HomeCardGrid.tsx
@@ -19,7 +19,7 @@ export default function HomeCardGrid({ memories }: { memories: Memory[] }) {
             <BidVertiserAd
               key="ad-infeed-home"
               rows={1}
-              imageWidth={200}
+              imageWidth={350}
               placement="home-infeed"
               variant="infeed"
             />
--- components/cards/VirtualizedCardGrid.tsx
+++ components/cards/VirtualizedCardGrid.tsx
@@ -248,7 +248,7 @@ export default function VirtualizedCardGrid({
         <BidVertiserAd
           key={`ad-archive-${globalIndex}`}
           rows={1}
-          imageWidth={200}
+          imageWidth={350}
           placement={`archive-${globalIndex}`}
           variant="infeed"
         />
